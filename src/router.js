9f3c72f3d88c64ab53b38307b55914f4853b1603
pattywitwhy <pattyxhwang@gmail.com>
2019-07-31 15:02:59 -0500

    fixed routes

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from './views/Home.vue'
 import Signup from "./views/Signup.vue"
 import Signout from "./views/Signout.vue"
 import Login from "./views/Login.vue"
+import GWC from "./views/GWC"
 
 Vue.use(Router)
 
@@ -11,10 +12,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', name: 'home', component: Home },
+    { path: '/', name: 'login', component: Login },
     { path: '/about', name: 'about'},
     { path: '/signup', name: 'signup', component: Signup },
     { path: '/signout', name: 'signout', component: Signout },
-    { path: '/Login', name: 'Login', component: Login }
+    { path: '/Login', name: 'Login', component: Login },
+    { path: '/GWC', name: 'GWC', component: GWC }
   ]
 })
--- src/views/Signout.vue
+++ src/views/Signout.vue
@@ -1,1 +1,23 @@
-SignOut.vue
+<template>
+  <div class="logout">
+    <h1>{{ Bye }}</h1>
+  </div>
+</template>
+
+<style>
+  h1  {
+    color: blue;
+  }
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  created: function() {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    this.$router.push("/login");
+  }
+};
+</script>
