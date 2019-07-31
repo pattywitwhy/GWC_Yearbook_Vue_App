f642b6d56718fe4081447f211debe0b6b4582777
pattywitwhy <pattyxhwang@gmail.com>
2019-07-31 14:43:10 -0500

    Added pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup from "./views/Signup.vue"
+import Signout from "./views/Signout.vue"
+import Login from "./views/Login.vue"
 
 Vue.use(Router)
 
@@ -8,18 +11,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about'},
+    { path: '/signup', name: 'signup', component: Signup },
+    { path: '/signout', name: 'signout', component: Signout },
+    { path: '/Login', name: 'Login', component: Login }
   ]
 })
