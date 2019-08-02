5b4e8f0c384ad4ffaecc694fca399f55f3104a85
pattywitwhy <pattyxhwang@gmail.com>
2019-08-02 14:54:05 -0500

    displaying students

--- src/views/StudentsIndex.vue
+++ src/views/StudentsIndex.vue
@@ -2,19 +2,8 @@
   <div class="students-index">
     <h1>Meet The CHICAGO AT&T Girls Who Code</h1>
     <div>
-      <form v-on:submit.prevent="submit()">
-        <div class="students">
-          Students: <select list="students">
-            <option v-for="student in students" v-bind:value="student.id">{{ student.first_name }}</option>
-            <h1>{{ student.first_name}}</h1>
-          </select>
-        </div>
-      </form>
-    </div>
-
-    <div>
       <div v-for="student in students" >
-        <router-link class="students-list" :to="'students/'">
+        <router-link v-bind:to="'students/' + student.id">
           {{ student.first_name + student.last_name }} 
         </router-link>
       </div>
@@ -33,12 +22,26 @@
     data: function() {
       return {
         students: [],
+        student: {
+                  first_name: "",
+                  last_name: "",
+                  birthday: "",
+                  age: "",
+                  school: "",
+                  grade: "",
+                  college: "",
+                  major: "",
+                  bio: "",
+                  final_project: ""
+        }
       };
     },
     created: function() {
       axios.get("/api/students")
         .then(response => {
           console.log(response.data)
+          this.students = response.data;
+
           // axios.get("/api/students")
           //   .then(response => {
           //     this.users = response.data
@@ -46,6 +49,26 @@
         });
     },
     methods: {
+      submit: function() {
+        console.log("Showing students")
+        var params = {
+                      first_name: this.student.first_name,
+                      last_name: this.student.last_name,
+                      birthday: this.student.birthday,
+                      age: this.student.age,
+                      school: this.student.school,
+                      grade: this.student.grade,
+                      college: this.student.college,
+                      major: this.student.major,
+                      bio: this.student.bio,
+                      final_project: this.student.final_project
+                      };                  
+
+          axios.post("api/students", params)
+            .then(response => {
+              this.$router.push("/students");
+            });
+      },
       storeID: function(userID) {
         localStorage.setItem("inviteId", userID);
         console.log(userID)
